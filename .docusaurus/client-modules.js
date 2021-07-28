7f338eed92b93f3a3dce2bcaa0c41f8797bee20d
acsamson <78223243+acsamson@users.noreply.github.com>
2021-07-29 05:19:47 +0800

    修改基础数据

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("/Users/samson/code/cshome/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/samson/code/cshome/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/samson/code/cshome/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/samson/code/cshome/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/samson/code/cshome/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/samson/code/cshome/src/css/custom.css"),
+];
